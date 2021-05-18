d01db04de079557f1eb2133b26551f72548147b0
royalts1011 <falco.lentzsch@gmail.com>
2021-05-18 12:09:10 +0200

    upload,save image + render image

--- scenario_1/server.py
+++ scenario_1/server.py
@@ -1,7 +1,18 @@
 from flask import Flask, render_template, request
 import os
+from werkzeug.utils import secure_filename
+from PIL import Image
+
+
+
+
+
+
+
 app = Flask(__name__)
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+UPLOAD_FOLDER = os.path.join(APP_ROOT, 'static')
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 '''
 Tutorial help: https://www.tutorialspoint.com/flask/flask_file_uploading.htm
@@ -13,7 +24,9 @@ def start():
 	
 @app.route("/selected_image", methods=['POST'])
 def select_query_image():
-    pass
+    f = request.files['file']
+    f.save(os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(f.filename)))
+    return render_template('show_image.html', uploaded_image=secure_filename(f.filename))
 		
 if __name__ == '__main__':
    app.run(debug = True)
